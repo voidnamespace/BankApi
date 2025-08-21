8b78748331fba2379644e1c70e4dede25b7e0d86
voidnamespace <voidnamespace@outlook.com>
2025-08-22 02:55:18 +0300

    Prepared for pull-request feature/entities

--- BankApi/Entities/Transaction.cs
+++ BankApi/Entities/Transaction.cs
@@ -6,9 +6,11 @@ public class Transaction
     public Guid Id { get; set; }
     public Guid? FromCardId { get; set; }
     public BankCard? FromCard { get; set; }
+   
     public Guid? ToCardId { get; set; }
     public BankCard? ToCard { get; set; }
-    public decimal Amount { get; set; }      
+
+    public decimal Amount { get; set; } 
     public DateTime Date { get; set; } = DateTime.UtcNow; 
     public TransactionType Type { get; set; } 
 }
